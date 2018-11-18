3a46bd67124be33a2de1714526c461004e49ac2d
Tushar Gupta <tushargpt224@gmail.com>
2018-11-18 16:03:31 +0530

    Update code.sol

--- solidity/code.sol
+++ solidity/code.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.10;
 
-contract article
+contract owner
+{
+    address owned_by;
+    function who_owner(address _owned_by) public
+    {
+        _owned_by = msg.sender;
+        owned_by = _owned_by;
+    }
+    modifier onlyowner
+    {
+        require(msg.sender == owned_by);
+        _;
+    }
+}
+
+contract article is owner
 {
     string article_name;
     string article_topic;
     uint article_word_limit;
 
-    function get_details(string _name,string _topic,uint _wt) public
+    function get_details(string _name,string _topic,uint _wt) onlyowner public
     {
         article_name = _name;
         article_topic = _topic;
